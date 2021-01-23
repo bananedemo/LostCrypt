cc9639735ad0ef8c2a40777a48fbeb4b50d9d918
YAEGASHI Takeshi <t-yaegashi@bns-g.com>
2021-01-23 19:59:36 +0900

    Update UnityTunnel.ps1

--- AzureDevOps/Scripts/UnityTunnel.ps1
+++ AzureDevOps/Scripts/UnityTunnel.ps1
@@ -1,9 +1,16 @@
 [CmdletBinding()]
 param(
-    [Parameter(Mandatory)][string]$Server,
-    [Parameter(Mandatory)][int]$Port = 58765,
-    [Parameter(Mandatory)][string]$Connect = '127.0.0.1:8080',
-    [Parameter(Mandatory)][string]$KeyPath
+    [Parameter(Mandatory)]
+    [string]$Server,
+
+    [int]$Port = 58765,
+
+    [string]$Connect = '127.0.0.1:8080',
+
+    [Parameter(Mandatory)]
+    [string]$KeyPath,
+
+    [switch]$UpdateConfig = $false
 )
 
 Set-StrictMode -Version 3
@@ -27,4 +34,32 @@ $ArgumentList = @(
     '-N'
 )
 
-Start-Process -NoNewWindow -FilePath ssh -ArgumentList $ArgumentList
+$proc = Start-Process -PassThru -NoNewWindow -FilePath ssh -ArgumentList $ArgumentList
+
+foreach ($i in 0..9) {
+    Write-Verbose "Waiting for the tunnel open ($i)"
+    $t = Test-NetConnection 127.0.0.1 -Port $Port
+    if ($t.TcpTestSucceeded) {
+        break
+    }
+    if ($proc.HasExited -or $i -eq 9) {
+        Stop-Process $proc -ea 0
+        throw "Unable to open the tunnel"
+    }
+    Start-Sleep -Seconds 1
+}
+
+$config = @"
+{
+    "licensingServiceBaseUrl": "http://127.0.0.1:${Port}",
+    "enableEntitlementLicensing": true,
+    "enableFloatingApi": true,
+    "clientConnectTimeoutSec": 5,
+    "clientHandshakeTimeoutSec": 10
+}
+"@
+
+if ($UpdateConfig) {
+    mkdir C:\ProgramData\Unity\config -ea 0 | Out-Null
+    $config | Set-Content -Path C:\ProgramData\Unity\config\services-config.json
+}
